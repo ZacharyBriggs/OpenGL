1d3cf32d12cdc673e747efc81e368e4766e778c8
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-09-18 12:56:23 -0500

    pre lunch

--- 3.GUI/GUIApplication.cpp
+++ 3.GUI/GUIApplication.cpp
@@ -11,10 +11,10 @@ void GUIApplication::startup()
 	m_mesh = new MeshRenderer();
 	m_defaultShader = new Shader();
 	m_transform = new Transform();
-	Vertex a = Vertex(glm::vec4(-5, 5, 0, 1), glm::vec4(1, 0, 0, 1));
-	Vertex b = Vertex(glm::vec4(5, 5, 0, 1), glm::vec4(0, 1, 0, 1));
-	Vertex c = Vertex(glm::vec4(5, -5, 0, 1), glm::vec4(0, 0, 1, 1));
-	Vertex d = Vertex(glm::vec4(-5, -5, 0, 1), glm::vec4(0, 0, 0, 1));
+	Vertex a = Vertex(glm::vec4(-10, 10, 0, 1), glm::vec4(1, 0, 0, 1));
+	Vertex b = Vertex(glm::vec4(10, 10, 0, 1), glm::vec4(0, 1, 0, 1));
+	Vertex c = Vertex(glm::vec4(10, -10, 0, 1), glm::vec4(0, 0, 1, 1));
+	Vertex d = Vertex(glm::vec4(-10, -10, 0, 1), glm::vec4(0, 0, 0, 1));
 	std::vector<Vertex> vertices = { a,b,c,d };
 	std::vector<unsigned int> indices = { 0 ,1,2,2,3,0 };
 	m_transform->SetModel(glm::mat4(1));
@@ -42,20 +42,20 @@ void GUIApplication::draw()
 		m_transform->Translate(glm::vec3(pos[0], pos[1], pos[2]));
 	m_defaultShader->bind();
 	int handle = m_defaultShader->getUniform("ProjectionViewWorld");
-	int yPos = -70;
+	int yPos = 70;
 	int xMultiple = 0;
 	for(int x = 1; x <= 64; x++)
 	{
 		glm::mat4 mat = glm::mat4(1);
 		mat = glm::translate(mat,glm::vec3(-100, 0, 0));
-		mat = glm::translate(mat, glm::vec3(10  * xMultiple, yPos, 0));
+		mat = glm::translate(mat, glm::vec3(20  * xMultiple, yPos, 0));
 		glm::mat4 mvp = m_projection * m_view* mat;
 		glUniformMatrix4fv(handle, 1, GL_FALSE, &mvp[0][0]);
 		m_mesh->render();
 		xMultiple++;
 		if(x%8 == 0)
 		{
-			yPos += 10;
+			yPos -= 20;
 			xMultiple = 0;
 		}
 	}
